6ff5b189343c99837e8724679fe77f1321ba571a
joj123123 <53939100+joj123123@users.noreply.github.com>
2020-01-16 16:19:09 +0900

    Update 7568_jojaegyu.cpp

--- [20200108] Ch06(brute-force) 10870.fibonacci, 11729.hanoiTower, 7568.build/7568_jojaegyu.cpp
+++ [20200108] Ch06(brute-force) 10870.fibonacci, 11729.hanoiTower, 7568.build/7568_jojaegyu.cpp
@@ -14,13 +14,16 @@ int main(int argc, const char * argv[]) {
     for(int i=0; i<N; i++){
         scanf("%d %d",&arr[i].weight,&arr[i].height);
     }
+    //구조체 배열에 각각의 몸무게와 키를 입력
     for(int i=0; i<N; i++){
         for(int k=0; k<N; k++){
             if(arr[i].weight < arr[k].weight && arr[i].height < arr[k].height){
                 count += 1;
             }
         }
-        printf("%d ",count + 1);
+        //자신보다 덩치가 크면 count 를 1 증가시킴
+        printf("%d ",count + 1);//자신보다 덩치가 큰 명수만큼 등수가 밀리게 됨
         count = 0;
     }
+    
 }
