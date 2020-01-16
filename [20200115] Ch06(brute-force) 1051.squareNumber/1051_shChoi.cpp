de0ee7c36ae0df277d4e9aa5c8d71bf1b59a24c9
SeongHunChoi <55524021+huya22@users.noreply.github.com>
2020-01-16 17:46:26 +0900

    Update 1051_shChoi.cpp

--- [20200115] Ch06(brute-force) 1051.squareNumber/1051_shChoi.cpp
+++ [20200115] Ch06(brute-force) 1051.squareNumber/1051_shChoi.cpp
@@ -24,13 +24,14 @@ int main() {
 	for (int i = 0; i < n; i++) {                               //행을 확인하면서
 		for (int j = 0; j < m; j++) {                       //각 열을 확인한다
 			for (int k = 1; k < MIN; k++) {             //정사각형의 길이는 최솟값을 넘지 못하기 때문에 
-				if ((i + k < n) && (j + k < m) && Data[i][j] == Data[i + k][j] && Data[i][j] == Data[i][j + k] && Data[i][j] == Data[i + k][j + k])
+				if ((i + k < n) && (j + k < m) && Data[i][j] == Data[i + k][j] 
+				    && Data[i][j] == Data[i][j + k] && Data[i][j] == Data[i + k][j + k])
 
 //행과 열이 범위를 넘어가지 않는 선에서 첫번째 기준 점과 같은 열에 같은 숫자가 있고 같은 행에도 같은 숫자가 있으며 대칭점 또한 같은 값이면 tmp에 대입
 				
 					tmp = max(tmp, (k + 1));
 				
-				//tmp와 새로 대입대는 값과의 크기 비교로 크면 tmp에 큰 값을 다시 대입 (k+1인 이유는 첫번째 기준점을 포함해줘야 하기때문)
+//tmp와 새로 대입대는 값과의 크기 비교로 크면 tmp에 큰 값을 다시 대입 (k+1인 이유는 첫번째 기준점을 포함해줘야 하기때문)
 			}
 		}
 	}
